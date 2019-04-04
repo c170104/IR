8b4d0f5eea610bfc0fe2a125d75ce0a85382fcc4
ibenrawkss <benben-x3@hotmail.com>
2019-04-04 14:44:20 +0000

    Changed to Whoosh

--- IR/main/models.py
+++ IR/main/models.py
@@ -1,11 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class FanFiction(models.Model):
-    movie = models.CharField(max_length=255)
-    title = models.CharField(max_length=255)
-    author = models.CharField(max_length=255)
-    passage = models.TextField()
-
-    def __str__(self):
-        return "{} by {}".format(self.title, self.author)
---
+++ whooshscript.py
@@ -0,0 +1,38 @@
+from whoosh.index import create_in, open_dir
+from whoosh.fields import Schema, STORED, ID, KEYWORD, TEXT
+from whoosh.analysis import StemmingAnalyzer
+import os.path
+
+
+# Specify schema
+schema = Schema(
+            title=              TEXT(stored=True, analyzer=StemmingAnalyzer()),
+            author=             TEXT,
+            movie=              KEYWORD(stored=True),
+            url=                ID(stored=True),
+            passage=            TEXT(analyzer=StemmingAnalyzer()),
+            passage_summary=    STORED
+        )
+
+# Creates the index
+if not os.path.exists("index"):
+    os.mkdir("index")
+    ix = create_in("index", schema)
+else:
+    ix = open_dir("index")
+
+writer = ix.writer()
+
+writer.add_document(
+    title=u'Testing is this correction',
+    author=u'Bill Gates',
+    movie=u'Batman',
+    url=u'https://testingonetwo.com',
+    passage=u'Some super duper long ass passage that no one wants to read. I am having difficulties trying to think of what else to write.',
+    passage_summary=u'Some super duper long ass passage that no one wants to read.'
+)
+
+writer.commit()
+
+
+
