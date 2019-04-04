c037ff6f162ccb0728a54b23e305ecf7387f991a
ibenrawkss <benben-x3@hotmail.com>
2019-04-04 14:44:20 +0000

    Changed to Whoosh

---
+++ test.py
@@ -0,0 +1,26 @@
+import os
+from whoosh.index import open_dir
+from whoosh.qparser import QueryParser
+
+if not os.path.exists("index"):
+    exit
+
+ix = open_dir("index")
+
+#with ix.searcher() as searcher:
+# print(list(ix.searcher().lexicon("passage")))  
+# print(list(ix.searcher().lexicon("movie")))
+# print(list(ix.searcher().lexicon("title")))
+
+index = "passage"
+search_query = "long"
+
+qp = QueryParser(index, schema=ix.schema)
+query = qp.parse(search_query)
+
+with ix.searcher() as searcher:
+    results = searcher.search(query)
+    print(results[0])
+
+
+
