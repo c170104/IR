14f37d73213a040680115a5aed7bc3860fb486fd
Lawrann Chan <lawrannc@gmail.com>
2019-04-05 02:11:51 +0800

    Scrapper

--- whooshscript.py
+++ whooshscript.py
@@ -2,6 +2,9 @@ from whoosh.index import create_in, open_dir
 from whoosh.fields import Schema, STORED, ID, KEYWORD, TEXT
 from whoosh.analysis import StemmingAnalyzer
 import os.path
+import json, os
+
+wd = os.getcwd() +"\\scrappedata\\"
 
 
 # Specify schema
@@ -23,15 +26,36 @@ else:
 
 writer = ix.writer()
 
-writer.add_document(
-    title=u'Testing is this correction',
-    author=u'Bill Gates',
-    movie=u'Batman',
-    url=u'https://testingonetwo.com',
-    passage=u'Some super duper long ass passage that no one wants to read. I am having difficulties trying to think of what else to write.',
-    passage_summary=u'Some super duper long ass passage that no one wants to read.'
-)
-
+count = 0
+#keys Movie, Title, Author, Hyperlink, Passage, Summary
+while(True):
+    txtfile = (wd+"textfile%i.txt" % (count))
+    if os.path.exists(txtfile):
+        with open(txtfile, 'r', encoding ="utf-8") as txt_file:
+            datastore = json.load(txt_file)
+            for i in range(len(datastore)):
+                t = datastore[i]["Title"]
+                a = datastore[i]["Author"]
+                m = datastore[i]["Movie"]
+                h = datastore[i]["Hyperlink"]
+                p = datastore[i]["Passage"]
+                s = datastore[i]["Summary"]
+                
+                # print(s.encode("utf-8").decode("utf-8"))
+                
+                writer.add_document (
+                title = str(t, 'utf-8'),
+                author = str(a, 'utf-8'),
+                movie = str(m, 'utf-8'),
+                url = str(h, 'utf-8'),
+                passage = str(p, 'utf-8'),
+                passage_summary = str(s, 'utf-8'),
+                )
+                
+        count = count + 1
+    else:
+        break
+            
 writer.commit()
 
 
